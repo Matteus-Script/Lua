0c1925c88d6bdccfa73e171ffc1cfb9f7fa9861e
Matteus-Script <green.boy@live.nl>
2024-10-06 18:04:15 +0200

    Update FortJewelry.lua

--- FortJewelry.lua
+++ FortJewelry.lua
@@ -1,70 +1,95 @@
---[[
-# Script Name:   <Fort Smelter>
-# Description:   <Smelts bars and banks gems at the fort>
-# Author:        <Matteus>
-# Version:       <2.1>
-# Date:          <2024.08.11>
---]]
-
 local API = require('api')
 
 local MAX_IDLE_TIME_MINUTES = 5
-local startTime, afk = os.time(), os.time()
-
 local furnaceID = 125137
 local BANK_CHEST = 125115
-local GEM_ID = 1615
+local GEM_ID = 4155 --1615 change this to whatever gem
+
+local SMITHING_IFACE = InterfaceComp5:new(37, 17, -1, -1, 0)
+
+local afk = os.time()
 
 API.SetDrawTrackedSkills(true)
 
-local function idleCheck()
+local function handleRandomEventsAndIdle()
+    API.DoRandomEvents()
+
     local timeDiff = os.difftime(os.time(), afk)
     local randomTime = math.random((MAX_IDLE_TIME_MINUTES * 60) * 0.6, (MAX_IDLE_TIME_MINUTES * 60) * 0.9)
-
     if timeDiff > randomTime then
         API.PIdle2()
         afk = os.time()
     end
 end
 
+local function isSmithingInterfacePresent()
+    local success, result = pcall(function()
+        return API.ScanForInterfaceTest2Get(true, { SMITHING_IFACE })
+    end)
+    
+    if success then
+        return #result > 0
+    else
+        return false
+    end
+end
+
 local function smeltMaterials()
-    API.logDebug("Starting smelting process")
     API.DoAction_Object1(0x3f, API.OFF_ACT_GeneralObject_route0, { furnaceID }, 50)
     API.RandomSleep2(2000, 2500, 3500)
-    API.KeyboardPress32(0x20, 0)
+
+    if isSmithingInterfacePresent() then
+        API.KeyboardPress32(0x20, 0)  -- Press spacebar
+    end
+
     API.RandomSleep2(450, 500, 550)
 end
 
 local function bankGems()
-    API.logDebug("Banking gems")
     API.DoAction_Object1(0x33, API.OFF_ACT_GeneralObject_route3, { BANK_CHEST }, 50)
     API.RandomSleep2(350, 400, 450)
 end
 
-local function checkInventoryForGems()
+local function hasGemsInInventory()
     return API.InvItemcount_1(GEM_ID) > 0
 end
 
-while API.Read_LoopyLoop() do
-    if not (API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing()) then
-        idleCheck()
+local function waitForInventoryUpdate()
+    local maxWaitTime = 3000  -- Maximum wait time in milliseconds (3 seconds)
+    local checkInterval = 500  -- Interval between checks in milliseconds
+    local elapsedTime = 0
 
-        -- Only smelt once if gems are present
-        if checkInventoryForGems() then
-            smeltMaterials()
+    while elapsedTime < maxWaitTime do
+        API.RandomSleep2(checkInterval, checkInterval, checkInterval)
+        if hasGemsInInventory() then
+            return true
         end
-
-        -- Wait until all gems are smelted
-        while checkInventoryForGems() and API.Read_LoopyLoop() do
-            API.RandomSleep2(500, 600, 700)  -- Delay to periodically check inventory
-        end
-
-        -- Bank only after all gems are smelted and the loop is still running
-        if API.Read_LoopyLoop() then
-            bankGems()
-        end
+        elapsedTime = elapsedTime + checkInterval
     end
-    
-    API.DoRandomEvents()
+
+    return false
+end
+
+while API.Read_LoopyLoop() do
+    handleRandomEventsAndIdle()
+
+    if not (API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing()) then
+        if hasGemsInInventory() then
+            smeltMaterials()
+
+            while hasGemsInInventory() do
+                handleRandomEventsAndIdle()
+                API.RandomSleep2(500, 600, 700)
+            end
+        end
+
+        bankGems()
+
+        --if not waitForInventoryUpdate() then
+        --    print("No more gems in inventory, stopping the script.")
+        --    break
+        --end
+    end
+
     API.RandomSleep2(500, 500, 500)
 end
