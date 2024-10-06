0a70375f06857162d9c077ae4ff4f41a085c1a21
Matteus-Script <green.boy@live.nl>
2024-10-06 18:02:43 +0200

    Update Priff Urn.lua

--- Priff Urn.lua
+++ Priff Urn.lua
@@ -1,85 +1,73 @@
 --[[
-# Script Name:   Prifddinas Urn maker
-# Description:   Makes urns in Prifddinas.
-# Author:        Matteus
-# Version:       1.2
-# Date:          2024.07.19
+# Script Name:   <Urn Crafter (Ironman)>
+# Description:   <Makes Urns in Priff select your urn then start>
+# Author:        <Matteus>
+# Version:       <1.0>
+# Date:          <2024.08.26>
 --]]
 
--- Release Notes:
--- Version 1.00  : Initial release.
--- Version 1.10  : Changed wait times, Added invcheck for softclay so it doesnt always have to bank first
--- Version 1.20  : Will now do random events whilst making urns
-
-API = require("api")
-
-API.SetDrawTrackedSkills(true)
-
-state = 1
+local API = require("api")
 
 local MAX_IDLE_TIME_MINUTES = 5
 local BANK_CHEST = 92692
 local POTTERSWHEEL = 94062
-local PotterInterface = { InterfaceComp5.new(1371, 7, -1, -1, 0) }
 local SOFT_CLAY_ID = 1761
 local MIN_SOFT_CLAY_COUNT = 2
 
-local STATES = {
-    {
-        desc = "Banking",
-        callback = function() API.DoAction_Object1(0x33, API.OFF_ACT_GeneralObject_route3, { BANK_CHEST }, 50) end,
-        post = function() return API.InvFull_() end
-    },
-    {
-        desc = "Opening Potterswheel",
-        callback = function() API.DoAction_Object1(0x3e, API.OFF_ACT_GeneralObject_route0, { POTTERSWHEEL }, 50) end,
-        post = nil
-    },
-    {
-        desc = "Making urns",
-        callback = function()
-            API.DoAction_Interface(0xffffffff, 0xffffffff, 0, 1370, 30, -1, API.OFF_ACT_GeneralInterface_Choose_option)
-        end,
-        post = function() return #API.ScanForInterfaceTest2Get(true, PotterInterface) <= 0 end
-    }
-}
-
-local function validatePosition()
-    if #API.GetAllObjArray1({ BANK_CHEST, POTTERSWHEEL }, 50, { 0, 12 }) <= 0 then
-        print("Please start near the Pottery wheel or bank in Prifddinas!")
-        return false
-    end
-    return true
-end
+API.SetDrawTrackedSkills(true)
 
 local function hasEnoughSoftClay()
     return (API.InvItemcount_1(SOFT_CLAY_ID) or 0) >= MIN_SOFT_CLAY_COUNT
 end
 
--- Main loop.
-while API.Read_LoopyLoop() and validatePosition() do
+local function getSelectedItemId()
+    return API.VB_FindPSettinOrder(1170, 0).state
+end
+
+local function isOpen()
+    return getSelectedItemId() ~= -1 and (API.Compare2874Status(18, false) or API.Compare2874Status(40, false))
+end
+
+local function waitForPotteryInterface()
+    local waitCounter = 0
+    local maxWait = 50
+    while waitCounter < maxWait do
+        if isOpen() then
+            return true
+        end
+        API.RandomSleep2(100, 200, 300)
+        waitCounter = waitCounter + 1
+    end
+    return false
+end
+
+--main loop
+while API.Read_LoopyLoop() do
     API.DoRandomEvents()
     API.SetMaxIdleTime(MAX_IDLE_TIME_MINUTES)
 
-  STATES[state].callback()
-
-    if not (API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing()) then
-        if state == 1 and hasEnoughSoftClay() then
-            -- Skip banking if there are at least 2 soft clay.
-            state = 2
-        elseif state == 2 and not hasEnoughSoftClay() then
-            -- If we don't have enough soft clay, switch to banking.
-            state = 1
-        end
-    
-        STATES[state].callback()
-    
-        if STATES[state].post and not STATES[state].post() then
-            print(STATES[state].desc .. " failed!")
-            break
-        end
-    
-        state = (state % #STATES) + 1
+    if API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing() then
+        API.RandomSleep2(200, 300, 400)
+        goto continue
     end
+
+    if not hasEnoughSoftClay() then
+        if not API.InvFull_() then
+            API.DoAction_Object1(0x33, API.OFF_ACT_GeneralObject_route3, { BANK_CHEST }, 50)
+            API.RandomSleep2(200, 300, 400)
+        end
+    end
+
+    if hasEnoughSoftClay() then
+        API.DoAction_Object1(0x3e, API.OFF_ACT_GeneralObject_route0, { POTTERSWHEEL }, 50)
+
+        if waitForPotteryInterface() then
+            API.KeyboardPress32(0x20, 0)
+        else
+            print("Failed to open the pottery interface.")
+        end
+    end
+
+    ::continue::
     API.RandomSleep2(200, 300, 400)
 end
