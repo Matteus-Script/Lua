6baf8909885662e5b86894eb4abcfd8a27b8e179
Matteus-Script <green.boy@live.nl>
2024-10-06 18:01:46 +0200

    Update Goldminer.lua

--- Goldminer.lua
+++ Goldminer.lua
@@ -1,21 +1,49 @@
 --[[
-# Script Name:   <LRC Gold Miner>
-# Description:   <Mines gold in lrc>
-# Author:        <Matteus>
-# Version:       <1.0>
-# Date:          <2024.08.10>
+# Script Name:   LRC Gold Miner Enhanced
+# Description:   Automates gold mining in LRC
+# Author:        Matteus
+# Version:       1.2
+# Date:          2024.09.01
 --]]
 
 local API = require('api')
 
+-- Configurable Parameters
 local MAX_IDLE_TIME_MINUTES = 5
+local ELVEN_SHARD_PRAYER_THRESHOLD = 50 
+local GOTE_RECHARGE_THRESHOLD = 500      
+local MIN_RECLICK_TIME = 25  
+local MAX_RECLICK_TIME = 35  
+
 local startTime, afk = os.time(), os.time()
+local nextMiningTime = nil  
 
 API.SetDrawTrackedSkills(true)
 
+-- Item IDs
 local IDS = {
-    ELVEN_SHARD = 43358
+    ELVEN_SHARD = 43358,
+    POTION_BUFF = 33234,
+    GOTE = 51490,
 }
+
+local POTION_IDS = {33234, 33232, 33230, 33228, 33226, 33224}
+
+local function checkAndDrinkPotion()
+    local buffStatus = API.Buffbar_GetIDstatus(IDS.POTION_BUFF, false)
+
+    if not buffStatus.found then
+        for _, potionId in ipairs(POTION_IDS) do
+            local potionCount = API.InvItemcount_1(potionId)
+            if potionCount > 0 then
+                API.DoAction_Inventory1(potionId, potionId, 1, API.OFF_ACT_GeneralInterface_route)
+                API.RandomSleep2(500, 1000, 1500)
+                return true
+            end
+        end
+    end
+    return false
+end
 
 local function hasElvenRitualShard()
     return API.InvItemcount_1(IDS.ELVEN_SHARD) > 0
@@ -32,76 +60,63 @@ local function idleCheck()
 end
 
 local function useElvenRitualShard()
-    if not hasElvenRitualShard() then return end
+    if not hasElvenRitualShard() then 
+        return 
+    end
+
     local prayer = API.GetPrayPrecent()
     local elvenCD = API.DeBuffbar_GetIDstatus(IDS.ELVEN_SHARD, false)
-    if prayer < 50 and not elvenCD.found then
-        API.logDebug("Using Elven Shard")
+
+    if prayer < ELVEN_SHARD_PRAYER_THRESHOLD and not elvenCD.found then
         API.DoAction_Inventory1(IDS.ELVEN_SHARD, IDS.ELVEN_SHARD, 1, API.OFF_ACT_GeneralInterface_route)
         API.RandomSleep2(600, 600, 600)
     end
 end
 
 local function keepGOTEcharged()
-    local buffStatus = API.Buffbar_GetIDstatus(51490, false)
+    local buffStatus = API.Buffbar_GetIDstatus(IDS.GOTE, false)
     local stacks = tonumber(buffStatus.text)
 
     local function findPorters()
-        local portersIds = {51490, 29285, 29283, 29281, 29279, 29277, 29275}
+        local portersIds = {IDS.GOTE, 29285, 29283, 29281, 29279, 29277, 29275}
         local porters = API.CheckInvStuff3(portersIds)
-        local foundIdx = -1
         for i, value in ipairs(porters) do
             if tostring(value) == '1' then
-                foundIdx = i
-                break
+                return portersIds[i]
             end
         end
-        if foundIdx ~= -1 then
-            local foundId = portersIds[foundIdx]
-            if foundId <= 51490 then
-                return foundId
-            else
-                return nil
-            end
-        else
-            return nil
-        end
+        return nil
     end
     
-    if stacks and stacks <= 50 and findPorters() then
-        API.logDebug("Recharging GOTE")
-        API.DoAction_Interface(0xffffffff, 0xae06, 6, 1464, 15, 2, API.OFF_ACT_GeneralInterface_route2)
-        API.RandomSleep2(600, 600, 600)
+    if stacks and stacks <= GOTE_RECHARGE_THRESHOLD then
+        local porterId = findPorters()
+        if porterId then
+            API.DoAction_Ability("Grace of the elves", 5, API.OFF_ACT_GeneralInterface_route)
+            API.RandomSleep2(600, 600, 600)
+        end
     end
 end
 
 local function performMining()
-    API.logDebug("Checking if Mining Action can be performed")
-    
-    
-    if not (API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing()) then
-        API.logDebug("Performing Mining Action")
-        
-        
+    local currentTime = os.time()
+    local isPlayerIdle = not (API.CheckAnim(50) or API.ReadPlayerMovin2() or API.isProcessing())  
+
+    if isPlayerIdle or (nextMiningTime == nil or currentTime >= nextMiningTime) then
         local ids = {113010}
-        
-        API.DoAction_Object1(0x3a, API.OFF_ACT_GeneralObject_route0, ids, 50)
-        
-        API.RandomSleep2(1000, 1500, 1000)
-    else
-        API.logDebug("Mining action skipped due to ongoing activity")
+        API.DoAction_Object1(0x3a, API.OFF_ACT_GeneralObject_route0, ids, 50) 
+        API.RandomSleep2(1000, 1500, 1000) 
+
+        nextMiningTime = currentTime + math.random(MIN_RECLICK_TIME, MAX_RECLICK_TIME)
     end
 end
 
+-- Main loop
 while API.Read_LoopyLoop() do
     idleCheck()
-
     useElvenRitualShard()
-
+    checkAndDrinkPotion()
     keepGOTEcharged()
-    
-    performMining()
-    
+    performMining()  
     API.DoRandomEvents()
     API.RandomSleep2(500, 500, 500)
 end
