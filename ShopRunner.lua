8bbed0f21e11e8ae99d7e821b9e8c7f0c5436aef
Matteus-Script <green.boy@live.nl>
2025-07-28 20:39:15 +0200

    Update ShopRunner.lua

--- ShopRunner.lua
+++ ShopRunner.lua
@@ -1,7 +1,7 @@
 -- Title: Shoprunner
 -- Author: <Matteus>
 -- Description: <Buys Runes from pretty much every runeshop + meat at Ooglog + mines crystal sandstone and red sandstone>
--- Version: <1.3.3>
+-- Version: <1.3.4>
 -- Category: Dailies
 -- Date : 2025.03.24
 
